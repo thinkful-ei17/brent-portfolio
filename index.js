8b72974a6e6bf57fdbb31867316a7a749e031384
Brent Guistwite <brentguistwite@gmail.com>
2018-02-09 16:37:11 -0500

    move jquery dom elements to index html file

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
-const content = {
-  about: {
-    text: 'I am a web developer and recent graduate of the Front End Web Development Career Path bootcamp at Thinkful.I love coding because solving problems and seeing ideas come to life on the page is supremely satisfying.I geek out on higher order functions, finding and learning awesome libraries or frameworks, and getting front and back ends to play nicely. When Im not coding, Im often found on Skype working with clients, curled up in a cozy chair reading, hiking in a canyon, or upside down doing a headstand.I am lifelong learner and my current learning projects include piano and writing my first book.',
-    image: './images/about-photo.jpg',
-  },
-};
-console.log(content.about.image);
-const modalCreator = className => (
-  `  
-   <div class=main-modal>
-    <article id="${className}">
-      <h2>${className}</h2>
-      <span>
-        <img class="${className}-image" src=${content[className].image}>
-      </span>
-      <p>
-       ${content[className].text}
-      </p>
-    </article>
-   </div>
-    `
-);
-
 const toggleBlur = () => {
   $('body').toggleClass('blurred');
 };
 
 
-const handleClicks = (className) => {
-  switch (className) {
-  case 'about':
-    $('#wrapper').html(`${modalCreator(className)}`);
-    break;
-  case 'portfolio':
-    $('#wrapper').html(`${modalCreator(className)}`);
-    break;
-  case 'contact':
-    $('#wrapper').html(`${modalCreator(className)}`);
-    break;
-  default: console.log('something went wrong :(');
-  }
-  toggleBlur();
-};
-
-
 $(() => {
-  $('.about').click((event) => {
-    handleClicks(event.target.className);
+  $('.about-box').click(() => {
+    $('.about').removeClass('hidden');
+    $('#header').addClass('hidden');
   });
 
-  $('.portfolio').click((event) => {
-    handleClicks(event.target.className);
+  $('.portfolio-box').click(() => {
+    $('.portfolio').removeClass('hidden');
+    $('#header').addClass('hidden');
   });
 
-  $('.contact').click((event) => {
-    handleClicks(event.target.className);
+  $('.contact-box').click(() => {
+    console.log('hello');
+    $('.contact').removeClass('hidden');
+    $('#header').addClass('hidden');
+  });
+
+  $('.go-back').click(() => {
+    $('#header').removeClass('hidden');
+    $('.modal').addClass('hidden');
   });
 });
